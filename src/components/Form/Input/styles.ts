e712f675f0e3377cd10b2647275b6d5345337413
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-01-13 18:31:32 -0300

    Improve UI on form fields

--- src/components/Form/Input/styles.ts
+++ src/components/Form/Input/styles.ts
@@ -2,10 +2,12 @@ import styled from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
+  height: 84px;
+  justify-content: center;
   border-bottom-width: 2px;
   border-style: solid;
   border-bottom-color: #ebebeb;
-  padding: ${RFValue(16)}px;
+  padding-left: ${RFValue(16)}px;
 `;
 
 export const Field = styled.TextInput`
@@ -14,6 +16,7 @@ export const Field = styled.TextInput`
   font-weight: bold;
   font-style: italic;
   color: #9d9d9d;
+  height: 100%;
 `;
 
 export const ErrorMessage = styled.Text`
@@ -21,4 +24,7 @@ export const ErrorMessage = styled.Text`
   font-weight: bold;
   font-style: italic;
   color: red;
+  position: absolute;
+  bottom: ${RFValue(12)}px;
+  left: ${RFValue(16)}px;
 `;
