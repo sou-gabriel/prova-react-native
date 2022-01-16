772d095729aaf449560000b0d16907cfdbe1ade9
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-01-16 10:54:23 -0300

    Create private routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,16 +1,39 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { HomeScreen } from "../screens/HomeScreen";
 import { DashboardScreen } from "../screens/DashboardScreen";
+import { AccountScreen } from "../screens/AccountScreen";
 
-const { Navigator, Screen } = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export const AppRoutes = () => {
-  return (
-    <Navigator>
-      <Screen name="Home" component={HomeScreen} />
-      <Screen name="Dashboard" component={DashboardScreen} />
-    </Navigator>
-  );
+const icons = {
+  Home: "home",
+  Dashboard: "dashboard",
+  Account: "account-circle",
 };
+
+export const AppRoutes = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused }) => {
+        const iconColor = focused ? "#fff" : "#000";
+
+        return (
+          <MaterialIcons name={icons[route.name]} size={24} color={iconColor} />
+        );
+      },
+      tabBarActiveTintColor: "#fff",
+      tabBarActiveBackgroundColor: "#4895ef",
+      tabBarLabelStyle: {
+        fontSize: 12,
+      },
+      headerShown: false,
+    })}
+  >
+    <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Screen name="Dashboard" component={DashboardScreen} />
+    <Tab.Screen name="Account" component={AccountScreen} />
+  </Tab.Navigator>
+);
