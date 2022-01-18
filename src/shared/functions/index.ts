51a6544aa803f0a4e67c102b8602dcb35e8ed47e
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-01-18 09:24:20 -0300

    Create functionality to format price

--- src/shared/functions/index.ts
+++ src/shared/functions/index.ts
@@ -4,3 +4,10 @@ export const showError = (error) => {
   console.log(error);
   Alert.alert(error.name, error.message);
 };
+
+export const getFormattedPrice = (price: number) => {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(price);
+};
