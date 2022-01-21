7bad44227a88fc063548160240d61ce61ed78fda
Gabriel Ramos <dev.gabrielramos@gmail.com>
2022-01-21 09:32:18 -0300

    Create interceptor to handle 401 status code

--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -1,3 +1,4 @@
+import { Alert } from "react-native";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -17,5 +18,17 @@ api.interceptors.request.use(async (config) => {
 
   return config;
 });
+
+api.interceptors.response.use((response) => {
+  return response
+}, (error) => {
+  const isAnUnauthorizedUser = error.response.status === 401
+
+  if (isAnUnauthorizedUser) {
+    Alert.alert('Usuário não cadastrado', 'Este usuário ainda não possui cadastro na aplicação.')
+  }
+
+  return Promise.reject(error.response)
+})
 
 export default api;
